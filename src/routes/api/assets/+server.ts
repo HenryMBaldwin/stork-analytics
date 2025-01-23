a741afa5abc4e3df3cc66f600d371a3cf426c7eb
Henry Baldwin <henry@stork.network>
2025-01-23 18:36:07 +0000

    Update +server.ts
    Use main endpoint

--- src/routes/api/assets/+server.ts
+++ src/routes/api/assets/+server.ts
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 
 export async function GET() {
 	try {
-		const response = await fetch('https://rest.dev.jp.stork-oracle.network/v1/prices/assets');
+		const response = await fetch('https://rest.jp.stork-oracle.network/v1/prices/assets');
 		const data = await response.json();
 		return json(data);
 	} catch (error) {
